afb3707e1319c90ca407f1b24fb5ff768c63a217
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-09-24 16:43:50 +0900

    ref #1 add package -> php_codesniffer

---
+++ vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-1-feat-development-env',
+        'version' => 'dev-1-feat-development-env',
+        'reference' => 'a3ad62a8ede85714e394f3e546bf04dca8e0bd34',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-1-feat-development-env',
+            'version' => 'dev-1-feat-development-env',
+            'reference' => 'a3ad62a8ede85714e394f3e546bf04dca8e0bd34',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'squizlabs/php_codesniffer' => array(
+            'pretty_version' => '3.7.2',
+            'version' => '3.7.2.0',
+            'reference' => 'ed8e00df0a83aa96acf703f8c2979ff33341f879',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../squizlabs/php_codesniffer',
+            'aliases' => array(),
+            'dev_requirement' => true,
+        ),
+    ),
+);
