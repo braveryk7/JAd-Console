60e382e436b6b4086b6a79a6c9c0064949c5410f
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-09-25 16:05:52 +0900

    refs #1 add wordpress develpment settings

---
+++ webpack.config.js
@@ -0,0 +1,52 @@
+const path = require( 'path' );
+const defaultConfig = require( '@wordpress/scripts/config/webpack.config' );
+
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
+module.exports = {
+	...defaultConfig,
+	entry: {
+		index: path.resolve( process.cwd(), 'src', 'index.tsx' ),
+	},
+	resolve: {
+		...defaultConfig.resolve,
+		alias: {
+			"src": path.resolve(__dirname, "./src"),
+		},
+		extensions: ['.ts', '.tsx', '.js']
+	},
+	cache: {
+		type: 'filesystem',
+		buildDependencies: {
+		  config: [ __filename ],
+		}
+	},
+	module: {
+		rules: [
+			...defaultConfig.module.rules,
+			{
+				test: /\.tsx?$/,
+				exclude: /node_modules/,
+				use: [
+					{
+						loader: 'thread-loader',
+						options: {
+							workers: require('os').cpus().length - 1,
+						}
+					},
+					{
+						loader: 'esbuild-loader',
+						options: {
+							loader: 'tsx',
+							target: 'es2015',
+						}
+					},
+				],
+			},
+		],
+	},
+	plugins: [
+		...defaultConfig.plugins,
+		new ForkTsCheckerWebpackPlugin(),
+	],
+};
