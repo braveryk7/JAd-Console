ff34270cc1119dd3ea6821d2241b2ddbc5da8a98
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-09-27 00:34:58 +0900

    refs #7 add judgment method

--- classes/class-jac-phpver-judge.php
+++ classes/class-jac-phpver-judge.php
@@ -13,3 +13,18 @@ declare( strict_types = 1 );
 if ( ! defined( 'ABSPATH' ) ) {
 	exit( 'You do not have access rights.' );
 }
+
+/**
+ * Judgment PHP Version.
+ */
+class Jac_Phpver_Judge {
+	/**
+	 * Judgment PHP version.
+	 *
+	 * @param string $version_received php version.
+	 * @return bool
+	 */
+	public function judgment( string $version_received ): bool {
+		return version_compare( PHP_VERSION, $version_received, '>=' ) ? true : false;
+	}
+}
