016fbae4b1c88636ec22e1ee822c7f0bc5953e35
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-09-27 00:31:46 +0900

    refs #7 initial commit

---
+++ classes/class-cxn-jc-phpver-judge.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Judgment PHP Version.
+ *
+ * @author Ken-chan
+ * @package WordPress
+ * @subpackage JAd Console
+ * @since 0.0.1
+ */
+
+declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
