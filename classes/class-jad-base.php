9b0bd3175e33d7912a6b5425d630bc3e1dee416f
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-09-27 12:26:44 +0900

    refs #9 initial commit

---
+++ classes/class-jad-base.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * JAd Console base class.
+ *
+ * @author Ken-chan
+ * @package WordPress
+ * @subpackage JAd Console
+ * @since 0.0.1
+ */
+
+declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
+
+/**
+ * JAd Console base class.
+ */
+class Jad_Base {}
