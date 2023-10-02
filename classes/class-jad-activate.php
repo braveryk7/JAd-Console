f44d6ac5e39b4f2b69e43fca245bbdf23e5c80a9
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-10-02 11:42:12 +0900

    refs #13 initial commit

---
+++ classes/class-jad-activate.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * JAd Console activate.
+ *
+ * @author Ken-chan
+ * @package WordPress
+ * @subpackage JAd Console
+ * @since 0.0.1
+ */
+
+declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
+
+/**
+ * JAd Console activate process.
+ */
+class Jad_Activate extends Jad_Base {
+}
