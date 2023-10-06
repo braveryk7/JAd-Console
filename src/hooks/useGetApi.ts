00939a7f048073f9324d5e89c0dcaf508d1c527e
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-10-06 15:38:26 +0900

    refs #11 add useGeApi default functions

---
+++ src/hooks/useGetApi.ts
@@ -0,0 +1,34 @@
+import { Dispatch, SetStateAction } from 'react';
+
+import apiFetch from '@wordpress/api-fetch';
+import { useEffect } from '@wordpress/element';
+
+import { apiType } from 'src/types/apiType';
+
+export const useGetApi = (
+	stateFunc: Dispatch< SetStateAction< apiType | undefined > >,
+	setApiError: Dispatch< SetStateAction< boolean > >,
+	setIsRinkerExists: Dispatch< SetStateAction< boolean > >,
+) => {
+	useEffect( () => {
+		apiFetch< apiType >(
+			{ path: '/jad-console/v1/options' }
+		).then( ( response ) => {
+			setApiError( false );
+			stateFunc( response );
+		} ).catch( () => {
+			setApiError( true );
+		} );
+	}, [ stateFunc, setApiError ] );
+
+	useEffect( () => {
+		apiFetch< boolean >(
+			{ path: '/jad-console/v1/get-rinker-activated' }
+		).then( ( response ) => {
+			setApiError( false );
+			setIsRinkerExists( response );
+		} ).catch( () => {
+			setApiError( true );
+		} );
+	}, [ setIsRinkerExists, setApiError ] );
+};
