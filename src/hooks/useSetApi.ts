9ab157fea8c6eda07ab0408943212c677f714a87
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-10-09 23:29:44 +0900

    refs #11 initial commit

---
+++ src/hooks/useSetApi.ts
@@ -0,0 +1,42 @@
+import apiFetch from '@wordpress/api-fetch';
+import { useContext, useEffect, useRef, useState } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
+
+import { apiContext } from 'src/index';
+
+import { apiType, useSetApiType } from 'src/types/apiType';
+
+export const useSetApi: useSetApiType = ( itemKey, value ) => {
+	const [ currentValue, setCurrentValue ] = useState< apiType | undefined >( value );
+
+	const {
+		apiData,
+		setNoticeValue,
+		setNoticeMessage,
+		snackbarTimer,
+	} = useContext( apiContext );
+
+	const isFirstRender = useRef( true );
+
+	useEffect( () => {
+		if ( isFirstRender.current ) {
+			isFirstRender.current = false;
+		} else if ( value && value !== currentValue ) {
+			setNoticeValue( undefined );
+			clearTimeout( snackbarTimer );
+			setCurrentValue( value );
+
+			apiFetch( {
+				path: '/jad-console/v1/update',
+				method: 'POST',
+				data: { [ itemKey ]: value[ itemKey ] },
+			} ).then( () => {
+				setNoticeValue( 'jad_success' );
+				setNoticeMessage( __( 'Success', 'jad-console' ) );
+			} ).catch( () => {
+				setNoticeValue( 'jad_error' );
+				setNoticeMessage( __( 'Error', 'jad-console' ) );
+			} );
+		}
+	}, [ apiData, itemKey, value, setNoticeMessage, setNoticeValue, snackbarTimer, currentValue ] );
+};
