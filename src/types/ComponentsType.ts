064024ba6b6bde1d3d09b98826d7059d577886e5
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-10-16 18:08:46 +0900

    refs #11 add TogglePropsType

--- src/types/ComponentsType.ts
+++ src/types/ComponentsType.ts
@@ -1,4 +1,11 @@
+import { itemKeyType } from 'src/types/apiType';
+
 export type TabItemsType = {
 	id: string,
 	title: string
+}
+
+export type TogglePropsType = {
+	itemKey: itemKeyType,
+	label: string,
 }
