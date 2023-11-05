f9037942a0d2be600dee35cba36f3b9c9d661535
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-11-05 11:57:35 +0900

    refs #11 add SaveStatusSnackbar component

---
+++ src/components/atoms/SaveStatusSnackbar.tsx
@@ -0,0 +1,44 @@
+import { css, cx } from '@emotion/css';
+
+import { Snackbar } from '@wordpress/components';
+
+import { SaveStatusSnackbarType } from 'src/types/ComponentsType';
+
+export const SaveStatusSnackbar = ( props: SaveStatusSnackbarType ) => {
+	const { noticeValue, noticeMessage } = props;
+
+	// noticeValueがjad-successの場合は緑色、jad-errorの場合は赤色にする
+	const noticeValueColor = noticeValue && noticeValue === 'jad_success' ? '#00a32a' : '#d63638';
+
+	const style = css`
+		background-color: ${ noticeValueColor };
+		animation-duration: 4s;
+		animation-iteration-count: 5;
+		animation-name: fade;
+		position: fixed;
+		right: 30px;
+		top: 50px;
+
+		@keyframes fade {
+			0% {
+				opacity: 0;
+			}
+
+			25% {
+				opacity: 0.7;
+			}
+
+			75% {
+				opacity: 0.7;
+			}
+
+			100% {
+				opacity: 0;
+			}
+		}
+	`;
+
+	return (
+		<Snackbar className={ cx( style, noticeValue ) }>{ noticeMessage }</Snackbar>
+	);
+};
