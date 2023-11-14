4b46cd76adb5849385d662af87f1df6fbddff3ae
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-11-14 13:59:47 +0900

    refs #24 initial commit

---
+++ classes/class-jad-ad-policy-notice.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * JAd Console activate.
+ *
+ * @author Ken-chan
+ * @package WordPress
+ * @subpackage JAd Console
+ * @since 0.0.1
+ */
+
+declare( strict_types = 1 );
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'You do not have access rights.' );
+}
+
+/**
+ * Class to output ad policy.
+ */
+class Jad_Ad_Policy_Notice {}
