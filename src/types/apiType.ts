ff8938c1cbd562a6e43ac44d8fa81f45137921c1
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-12-04 23:41:05 +0900

    refs #30 add notifyApiType

--- src/types/apiType.ts
+++ src/types/apiType.ts
@@ -6,6 +6,8 @@ export type apiType = {
 	policy_page_url: string;
 }
 
+export type notifyApiType = Pick<apiType, 'design_type' | 'main_message' | 'policy_page_url'>;
+
 export type itemKeyType = keyof apiType;
 
 export type useSetApiType = {
