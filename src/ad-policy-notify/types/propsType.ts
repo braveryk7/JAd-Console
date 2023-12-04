2bab00e13f2539e57e73b80ccfad0029cfbee34e
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-12-04 23:46:59 +0900

    refs #30 add apiData -> propsType

--- src/ad-policy-notify/types/propsType.ts
+++ src/ad-policy-notify/types/propsType.ts
@@ -1,6 +1,9 @@
 import { Dispatch, SetStateAction } from 'react';
 
+import { notifyApiType } from 'src/types/apiType';
+
 export type propsType = {
 	notifyHidden: boolean,
 	setNotifyHidden: Dispatch<SetStateAction< boolean > >
+	apiData: notifyApiType
 }
