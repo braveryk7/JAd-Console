f393b4172b69c1956f8b9302c9896253950889c2
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-12-04 23:41:51 +0900

    refs #30 add apiData, apiError -> useState, useGetApi

--- src/ad-policy-notify/index.tsx
+++ src/ad-policy-notify/index.tsx
@@ -2,13 +2,20 @@ import { createRoot, useState } from '@wordpress/element';
 
 import { MinimizerToggle } from 'src/ad-policy-notify/components/MinimizerToggle';
 import { ShowNotify } from 'src/ad-policy-notify/components/ShowNotify';
+import { useGetApi } from 'src/hooks/useGetApi';
+
+import { notifyApiType } from 'src/types/apiType';
 
 export const AdPolicyNotify = () => {
+	const [ apiData, setApiData ] = useState< notifyApiType | undefined >( undefined );
+	const [ apiError, setApiError ] = useState( false );
 	const [ notifyHidden, setNotifyHidden ] = useState( true );
+
+	useGetApi( 'notify', setApiData, setApiError );
 
 	return (
 		<>
-			{ notifyHidden ? (
+			{ ! apiError && apiData && notifyHidden ? (
 				<MinimizerToggle
 					notifyHidden={ notifyHidden }
 					setNotifyHidden={ setNotifyHidden }
