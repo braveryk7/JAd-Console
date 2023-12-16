bd4232db5e24249b5c45c768308930bfc5a39d94
braveryk7 <52683567+braveryk7@users.noreply.github.com>
2023-12-16 16:57:17 +0900

    refs #33 add itemCategory

--- src/types/ComponentsType.ts
+++ src/types/ComponentsType.ts
@@ -1,4 +1,4 @@
-import { itemKeyType } from 'src/types/apiType';
+import { itemCategoryType, itemKeyType } from 'src/types/apiType';
 import { noticeValueType } from 'src/types/contextType';
 
 export type TabItemsType = {
@@ -7,16 +7,19 @@ export type TabItemsType = {
 }
 
 export type TogglePropsType = {
+	itemCategory: itemCategoryType,
 	itemKey: itemKeyType,
 	label: string,
 }
 
 export type TextControlType = {
+	itemCategory: itemCategoryType,
 	itemKey: Extract< itemKeyType, string >,
 	label: string,
 }
 
 export type SelectControlType = {
+	itemCategory: itemCategoryType,
 	itemKey: Extract< itemKeyType, 'design_type' >,
 	label: string,
 }
